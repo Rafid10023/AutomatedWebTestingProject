c65543f7196b592ac29219ad2179be813f0bc3d8
Anthony Yanit <anyanit1@gmail.com>
2026-03-30 13:59:42 +0100

    Added Gherkin script, methods and tests for testing the Contact Us Page

---
+++ AutomatedWebTestingProject/src/test/java/webtests/pages/ContactUsPage.java
@@ -0,0 +1,67 @@
+package webtests.pages;
+
+
+import net.serenitybdd.core.annotations.findby.FindBy;
+import net.serenitybdd.core.pages.WebElementFacade;
+import net.thucydides.core.annotations.DefaultUrl;
+import net.thucydides.core.pages.PageObject;
+import org.openqa.selenium.Alert;
+
+@DefaultUrl("https://automationexercise.com/contact_us")
+public class ContactUsPage extends PageObject {
+
+    @FindBy(name = "name")
+    private WebElementFacade nameField;
+
+    @FindBy(name = "email")
+    private WebElementFacade emailField;
+
+    @FindBy(name = "subject")
+    private WebElementFacade subjectField;
+
+    @FindBy(name = "message")
+    private WebElementFacade messageField;
+
+    @FindBy(name = "upload_file")
+    private WebElementFacade chooseFileButton;
+
+    @FindBy(name = "submit")
+    private WebElementFacade submitButton;
+
+    @FindBy(xpath = "//div[@class='status alert alert-success']")
+    private WebElementFacade successVerification;
+
+
+    public void enterName(String name) {
+        nameField.type(name);
+    }
+
+    public void enterEmail(String email) {
+        emailField.type(email);
+    }
+
+    public void enterSubject(String subject) {
+        subjectField.type(subject);
+    }
+
+    public void enterMessage(String message) {
+        messageField.type(message);
+    }
+
+    public void clickChooseFileButton() {
+        chooseFileButton.click();
+    }
+
+    public void clickSubmitButton() {
+        submitButton.click();
+    }
+
+    public void accecptAlert() {
+        Alert alert = getDriver().switchTo().alert();
+        alert.accept();
+    }
+
+    public boolean getConfirmationMessage() {
+        return successVerification.isCurrentlyVisible();
+    }
+}
---
+++ AutomatedWebTestingProject/src/test/java/webtests/steps/ContactUsFormSteps.java
@@ -0,0 +1,58 @@
+package webtests.steps;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.thucydides.core.annotations.Managed;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import webtests.pages.ContactUsPage;
+
+public class ContactUsFormSteps {
+    @Managed
+    ContactUsPage contactUsPage;
+
+    @Given("I am on the Contact Us page")
+    public void iAmOnTheContactUsPage() {
+        contactUsPage.open();
+    }
+
+    @And("I have entered the name {string}")
+    public void iHaveEnteredTheName(String name) {
+        contactUsPage.enterName(name);
+    }
+
+    @And("I have entered the email address {string}")
+    public void iHaveEnteredTheEmail(String email) {
+        contactUsPage.enterEmail(email);
+    }
+
+
+    @And("I have entered the subject {string}")
+    public void iHaveEnteredTheSubject(String subject) {
+        contactUsPage.enterSubject(subject);
+    }
+
+    @And("I have entered the message {string}")
+    public void iHaveEnteredTheMessage(String message) {
+        contactUsPage.enterMessage(message);
+    }
+
+    @When("I click the Submit button")
+    public void iClickTheSubmitButton() {
+        contactUsPage.clickSubmitButton();
+    }
+
+    @And("I click the OK button")
+    public void iClickTheOKButton() {
+        contactUsPage.accecptAlert();
+    }
+
+    @Then("I should see the verification message \"Success! Your details have been submitter successfully'")
+    public void iShouldSeeTheVerificationMessageSuccessYourDetailsHaveBeenSubmitterSuccessfully(){
+        MatcherAssert.assertThat(contactUsPage.getConfirmationMessage(), Matchers.is(true));
+    }
+
+
+}
